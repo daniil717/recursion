1986e7de7853d145bccdc5dd1a0f7f2c63fb25f9
Daniil <D.aniil_2005>
2024-04-07 21:37:03 +0500

    ааа

--- src/Problems/Problem9.java
+++ src/Problems/Problem9.java
@@ -1,4 +1,21 @@
 package Problems;
 
+import java.util.Scanner;
+
 public class Problem9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter n and k:");
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        System.out.println("Answer:" + binomialCoefficient(n, k));
+        scanner.close();
+    }
+
+    public static int binomialCoefficient(int n, int k) {
+        if (k == 0 || k == n)
+            return 1;
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    }
 }
