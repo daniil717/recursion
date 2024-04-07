c5576e26868332163d9496c6a4c13a47c73e1c1b
Daniil <D.aniil_2005>
2024-04-07 20:30:24 +0500

    ааа

--- src/Problems/Problem3.java
+++ src/Problems/Problem3.java
@@ -1,4 +1,30 @@
 package Problems;
 
+import java.util.Scanner;
 public class Problem3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int number = scanner.nextInt();
+
+        if(isPrime(number)){
+            System.out.println(number + " Prime");
+        }else{
+            System.out.println(number + " Composite");
+        scanner.close();
+        }
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        return isPrimeUtil(n, 2);
+    }
+
+    private static boolean isPrimeUtil(int n, int i) {
+        if (i == n)
+            return true;
+        if (n % i == 0)
+            return false;
+        return isPrimeUtil(n, i + 1);
+    }
 }
--- src/Problems/Problem4.java
+++ src/Problems/Problem4.java
@@ -1,4 +1,11 @@
 package Problems;
 
+import java.util.Scanner;
 public class Problem4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n = scanner.nextInt();
+        System.out.println("Answer:" );
+    }
 }
