79088c1a7fea652d9acd2a205cc2d98f14ec064e
Daniil <D.aniil_2005>
2024-04-07 21:57:56 +0500

    ааа

--- src/Problems/Problem10.java
+++ src/Problems/Problem10.java
@@ -1,4 +1,22 @@
 package Problems;
 
+import java.util.Scanner;
+
 public class Problem10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number a and b:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("answer:" + gcd(a,b));
+        scanner.close();
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
 }
