8cfa602f17bc05a02532d01ff00859b29d98f745
Daniil <D.aniil_2005>
2024-04-07 20:04:42 +0500

    ааа

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 import Problems.Problem1;
@@ -9,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
+
 
         while (running) {
             System.out.println("Choose the number of task");
--- src/Problems/Problem1.java
+++ src/Problems/Problem1.java
@@ -1,4 +1,24 @@
 package Problems;
 
+import java.util.Scanner;
 public class Problem1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("array elements:");
+        for(int i = 0;i < n;i++){
+            array[i] = scanner.nextInt();
+        }
+        int min = array[0];
+        for(int i = 1;i < n;i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+
+        }
+        System.out.println("Minimum =" +min );
+        scanner.close();
+    }
 }
