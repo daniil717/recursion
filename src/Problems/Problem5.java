be226327c68b8ce2bfd802656faed385d6e165de
Daniil <D.aniil_2005>
2024-04-07 20:40:43 +0500

    ааа

--- src/Problems/Problem5.java
+++ src/Problems/Problem5.java
@@ -1,4 +1,21 @@
 package Problems;
 
+import java.util.Scanner;
+
 public class Problem5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter item number:");
+        int n = scanner.nextInt();
+        System.out.println("F"+n+" = " + fibonacci(n));
+        scanner.close();
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 }
