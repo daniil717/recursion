b6d1ef1ce921705c5ff215a3984dc3eff74be751
Daniil <D.aniil_2005>
2024-04-07 21:28:59 +0500

    ааа

--- src/Problems/Problem8.java
+++ src/Problems/Problem8.java
@@ -1,4 +1,22 @@
 package Problems;
 
+import java.util.Scanner;
+
 public class Problem8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the string:");
+        String s = scanner.next();
+
+        if(isDigits(s)){
+            System.out.println(s + " Yes");
+        }else{
+            System.out.println(s + " No");
+        }
+        scanner.close();
+    }
+
+    public static boolean isDigits(String s){
+        return s.matches("\\d+");
+    }
 }
