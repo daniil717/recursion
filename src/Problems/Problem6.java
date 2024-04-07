640f8f5e9c5f28738997c113f205f559dce9f3fd
Daniil <D.aniil_2005>
2024-04-07 20:45:46 +0500

    ааа

--- src/Problems/Problem6.java
+++ src/Problems/Problem6.java
@@ -1,4 +1,19 @@
 package Problems;
 
+import java.util.Scanner;
+
 public class Problem6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter numbers a and n:");
+        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        System.out.println("answer: " + power(a,n));
+    }
+    public static int power(int a, int n) {
+        if (n == 0)
+            return 1;
+        else
+            return a * power(a, n - 1);
+    }
 }
