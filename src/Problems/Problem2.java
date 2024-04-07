11bd1154d3654494c0a3436effb9b126438bb8e6
Daniil <D.aniil_2005>
2024-04-07 20:15:01 +0500

    ааа

--- src/Problems/Problem2.java
+++ src/Problems/Problem2.java
@@ -1,4 +1,24 @@
 package Problems;
 
+import java.util.Scanner;
 public class Problem2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("array elements:");
+        for(int i = 0;i < n;i++){
+            array[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for(int i = 0;i < n;i++){
+            sum += array[i];
+
+        }
+        double average = (double) sum / n;
+
+        System.out.println("avarage number:" + average);
+        scanner.close();
+    }
 }
