c9e30bfd6ca41007b1380b26a01e8a17f28846ab
Daniil <D.aniil_2005>
2024-04-07 21:20:13 +0500

    ааа

--- src/Problems/Problem7.java
+++ src/Problems/Problem7.java
@@ -1,4 +1,34 @@
 package Problems;
 
+import java.util.Scanner;
 public class Problem7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter n:");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("array elements:");
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        reverseArray(array, 0, n - 1);
+        System.out.println("reverse array:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+        scanner.close();
+    }
+
+    public static void reverseArray(int[] array, int start, int end) {
+        if (start >= end)
+            return;
+        int temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
+        reverseArray(array, start + 1, end - 1);
+    }
 }
