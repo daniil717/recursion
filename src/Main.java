6a9f6f2accb7dd81722c763864be33665c66a9b1
Daniil <D.aniil_2005>
2024-04-07 19:27:05 +0500

    ааа

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+import Problems.Problem1;
+import Problems.*;
+import Other.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Choose the number of task");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+}
+
